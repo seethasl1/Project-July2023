1620fe4c09186e3b0e66418521252b2169e98944
seethasl1 <87411968+seethasl1@users.noreply.github.com>
2023-07-03 11:01:51 +0530

    Update FirstPrg.cs
    class is added to this file

--- FirstPrg.cs
+++ FirstPrg.cs
@@ -5,12 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 namespace FirstPrg
 {
+    class Emp
+    {
+        public int Eid;
+        public string ename;
+        public override void Tostring()
+        {
+            System.Console.WriteLine(Eid + Ename);
+        }
+    }
 
     internal class Program
     {
         static void Main(string[] args)
         {
             System.Console.WriteLine("welcome to My Page");
+            
         }
     }
 }
